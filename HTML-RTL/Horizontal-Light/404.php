944094d6da0fd4c10a0e5153b50490c930bb5b0d
hananeakhouzam <59393585+hananeakhouzam@users.noreply.github.com>
2020-06-09 14:45:48 +0100

    add page 404

---
+++ HTML-RTL/Horizontal-Light/404.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+<?php
+
+include 'header.php';
+
+?>
+
+	<body class="main-body bg-primary-transparent">
+		
+		<!-- Page -->
+		<div class="page">
+		
+			<!-- Main-error-wrapper -->
+			<div class="main-error-wrapper  page page-h ">
+				<img src="../../assets/img/media/404.png" class="error-page" alt="error">
+				<h2>الصفحة غير موجودة.</h2>
+				<h6>ربما أن الرابط غير صحيح أو أن الصفحة غير موجودة.</h6><a class="btn btn-outline-danger" href="index.html">العودة للرئيسية</a>
+			</div>
+			<!-- /Main-error-wrapper -->
+		
+		</div>
+		<!-- End Page -->
+
+	
+<?php
+
+include 'footer.php';
+
+?>
+	</body>
+</html>
