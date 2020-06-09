5fbc75fedc65493c4ed473dce5dad7827aa7307b
hananeakhouzam <59393585+hananeakhouzam@users.noreply.github.com>
2020-06-09 13:43:53 +0100

    wip

--- HTML-RTL/Horizontal-Light/index.php
+++ HTML-RTL/Horizontal-Light/index.php
@@ -1,42 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
+<?php
 
-		<meta charset="UTF-8">
-		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="Description" content="Bootstrap Responsive Admin Web Dashboard HTML5 Template">
-		<meta name="Author" content="Spruko Technologies Private Limited">
-		<meta name="Keywords" content="admin,admin dashboard,admin dashboard template,admin panel template,admin template,admin theme,bootstrap 4 admin template,bootstrap 4 dashboard,bootstrap admin,bootstrap admin dashboard,bootstrap admin panel,bootstrap admin template,bootstrap admin theme,bootstrap dashboard,bootstrap form template,bootstrap panel,bootstrap ui kit,dashboard bootstrap 4,dashboard design,dashboard html,dashboard template,dashboard ui kit,envato templates,flat ui,html,html and css templates,html dashboard template,html5,jquery html,premium,premium quality,sidebar bootstrap 4,template admin bootstrap 4"/>
+include 'header.php';
 
-		<!-- Title -->
-		<title> Valex -  Premium dashboard ui bootstrap rwd admin html5 template </title>
-
-		<!-- Favicon -->
-		<link rel="icon" href="../../assets/img/brand/favicon.png" type="image/x-icon"/>
-
-		<!-- Icons css -->
-		<link href="../../assets/css/icons.css" rel="stylesheet">
-
-		<!--  Custom Scroll bar-->
-		<link href="../../assets/plugins/mscrollbar/jquery.mCustomScrollbar.css" rel="stylesheet"/>
-
-		<!--  Sidebar css -->
-		<link href="../../assets/plugins/sidebar/sidebar.css" rel="stylesheet">
-
-		<!--- Internal Morris css-->
-		<link href="../../assets/plugins/morris.js/morris.css" rel="stylesheet">
-
-		<!--- Style css --->
-		<link href="../../assets/css-rtl/style.css" rel="stylesheet">
-
-		<!--- Dark-mode css --->
-		<link href="../../assets/css-rtl/style-dark.css" rel="stylesheet">
-
-		<!---Skinmodes css-->
-		<link href="../../assets/css-rtl/skin-modes.css" rel="stylesheet" />
-
-	</head>
+?>
 
 	<body class="main-body">
 
@@ -407,7 +375,6 @@
 							<div class="card card-table-two">
 								<div class="d-flex justify-content-between">
 									<h4 class="card-title mb-1">الأرباح لهذا الشهر</h4>
-									<i class="mdi mdi-dots-horizontal text-gray"></i>
 								</div>
 								<span class="tx-12 tx-muted mb-3 ">حصيلة الأرباح لهذا الشهر.</span>
 								<div class="table-responsive country-table">
@@ -1106,77 +1073,10 @@
 		
 		</div>
 		<!-- End Page -->
-		
+		<?php 
+			include 'footer.php';
+			?>
 		<!-- Back-to-top -->
-		<a href="#top" id="back-to-top"><i class="las la-angle-double-up"></i></a>
-
-		<!-- JQuery min js -->
-		<script src="../../assets/plugins/jquery/jquery.min.js"></script>
-
-		<!-- Bootstrap Bundle js -->
-		<script src="../../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-		<!-- Ionicons js -->
-		<script src="../../assets/plugins/ionicons/ionicons.js"></script>
-
-		<!-- Moment js -->
-		<script src="../../assets/plugins/moment/moment.js"></script>
-
-		<!--Internal Sparkline js -->
-		<script src="../../assets/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
-
-		<!-- Moment js -->
-		<script src="../../assets/plugins/raphael/raphael.min.js"></script>
-
-		<!-- Internal Piety js -->
-		<script src="../../assets/plugins/peity/jquery.peity.min.js"></script>
-
-		<!--Internal  Flot js-->
-		<script src="../../assets/plugins/jquery.flot/jquery.flot.js"></script>
-		<script src="../../assets/plugins/jquery.flot/jquery.flot.pie.js"></script>
-		<script src="../../assets/plugins/jquery.flot/jquery.flot.resize.js"></script>
-		<script src="../../assets/plugins/jquery.flot/jquery.flot.categories.js"></script>
-		<script src="../../assets/js/dashboard.sampledata.js"></script>
-		<script src="../../assets/js/chart.flot.sampledata.js"></script>
-
-		<!-- Sticky js -->
-		<script src="../../assets/js/sticky.js"></script>
-
-		<!-- Rating js-->
-		<script src="../../assets/plugins/rating/jquery.rating-stars.js"></script>
-		<script src="../../assets/plugins/rating/jquery.barrating.js"></script>
-
-		<!-- P-scroll js -->
-		<script src="../../assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-		<script src="../../assets/plugins/perfect-scrollbar/p-scroll.js"></script>
-
-		<!-- Horizontalmenu js-->
-		<script src="../../assets/plugins/sidebar/sidebar-rtl.js"></script>
-		<script src="../../assets/plugins/sidebar/sidebar-custom.js"></script>
-
-		<!-- Eva-icons js -->
-		<script src="../../assets/js/eva-icons.min.js"></script>
-
-		<!--Internal Apexchart js-->
-		<script src="../../assets/js/apexcharts.js"></script>
-
-		<!-- Horizontalmenu js-->
-		<script src="../../assets/plugins/horizontal-menu/horizontal-menu-2/horizontal-menu.js"></script>
-
-		<!-- Internal Map -->
-		<script src="../../assets/plugins/jqvmap/jquery.vmap.min.js"></script>
-		<script src="../../assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-
-		<!-- Internal Chart js -->
-		<script src="../../assets/plugins/chart.js/Chart.bundle.min.js"></script>
-
-		<!--Internal  index js -->
-		<script src="../../assets/js/index.js"></script>
-		<script src="../../assets/js/jquery.vmap.sampledata.js"></script>
-
-		<!-- custom js -->
-		<script src="../../assets/js/custom.js"></script>
-		<script src="../../assets/js/jquery.vmap.sampledata.js"></script>
-
+		
 	</body>
 </html>
